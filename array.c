4cc6f536610774c3a3af6430027cfc5d84477189
gpbio10 <gpbio10@users.noreply.github.com>
2021-01-27 20:54:09 -0800

    Final

--- array.c
+++ array.c
@@ -3,40 +3,43 @@
 #include <math.h>
 #include <time.h>
 
-#define NUMBEROFPOINTS 100000
+//Can be used to calculate PI but has max value of 1000000.
+//Used linked list data structure for more accurate estimation of PI
 
-double randInRange() {
+#define NUMBEROFPOINTS 1000000
+
+float random() {
     return ((rand() % 10000) / 5000.0) - 1;
-}
+} //random()
 
-void printDataPoints(double x_coordinates[], double y_coordinates[]) {
+void printDataPoints(float x_coordinates[], float y_coordinates[]) {
     printf("Random Points between 0 and 1 to represent points on a graph for and domain and range of 0 to 1.\nX\t\t\tY\n");
     for (int i = 0; i < NUMBEROFPOINTS; i++) {
         printf("%f\t%f\n", x_coordinates[i], y_coordinates[i]);
     }
-}
+} //printDataPoints()
 
-int checkDistance(double x, double y) {
-    double sumOfSquares = (x*x) + (y*y);
+int checkDistance(float x, float y) {
+    float sumOfSquares = (x*x) + (y*y);
     //printf("%f\n", sumOfSquares);
     if (sqrt(sumOfSquares) <= 1.0) {
         return 1;
     } else {
         return 0;
     }
-}
+} //checkDistance()
 
 int main() {
 
-    double x_coordinate[NUMBEROFPOINTS];
-    double y_coordinate[NUMBEROFPOINTS];
+    float x_coordinate[NUMBEROFPOINTS];
+    float y_coordinate[NUMBEROFPOINTS];
 
     int numInsideCircle = 0.0;
 
     srand(time(0));
     for (int i = 0; i < NUMBEROFPOINTS; i++) {
-        x_coordinate[i] = randInRange();
-        y_coordinate[i] = randInRange();
+        x_coordinate[i] = random();
+        y_coordinate[i] = random();
 
         if (checkDistance(x_coordinate[i], y_coordinate[i]) == 1) {
             numInsideCircle++;
@@ -46,8 +49,8 @@ int main() {
     //printDataPoints(x_coordinate, y_coordinate);
 
     printf("%d\n", numInsideCircle);
-    double pi = 4 * (double)numInsideCircle / (double)NUMBEROFPOINTS;
+    float pi = 4 * (float)numInsideCircle / (float)NUMBEROFPOINTS;
 
     printf("%f", pi);
 
-}
+} //main()
--- linkedlist.c
+++ linkedlist.c
@@ -3,73 +3,138 @@
 #include <math.h>
 #include <time.h>
 
-#define NUMBEROFPOINTS 10
+#define NUMBEROFPOINTS 10000000
 
+/*  This is a pi predictor that uses linked lists
+ *  to remove the limit of data points that can be used
+ *  compared to that of an array. The NUMBEROFPOINTS is
+ *  set to the highest my computer could handle but I'm
+ *  sure 1-2 orders of magnitude could be added if you
+ *  have some decent computing power. This method of
+ *  prediction is a Monte Carlo Experiment which just
+ *  uses as many possible data points to simulate outcomes.
+ *
+ *  In this case two random x and y coordinates were generated
+ *  to represent a point on a graph with a domain and range of
+ *  -1 to 1. The magnitude of the line to the point from the
+ *  origin is then calculated to see what points would land
+ *  in a circle with a radius 1. Pi is finally calculated with
+ *  the equation (4 * Number of Points in the Circle) / (Total
+ *  Number of Points).
+ */
+
+
+//Created the Linked List Node
 struct Node {
-    int *data;
-
+    float x_coordinate;     //x and y coordinated stored at each node
+    float y_coordinate;
+    float distance;         //Value for euclidean distance
     struct Node *next;
 };
 
-void add(struct Node **head_ref, void *new_data, size_t data_size) {
+void add(struct Node **head, float new_x, float new_y) {
+    //Get memory for node with malloc
     struct Node *new_node = (struct Node*)malloc(sizeof(struct Node));
 
-    new_node->data = malloc(data_size);
-    new_node->next = (*head_ref);
+    //Set x and y coordinates to inputed data
+    new_node->x_coordinate = new_x;
+    new_node->y_coordinate = new_y;
 
-    int i;
-    for (i = 0; i < data_size; i++) {
-        *(char *)(new_node->data + i) = *(char *)(new_data + i);
+    //Next node
+    new_node->next = *head;
+
+    //Set pointer of head to new node
+    *head = new_node;
+} //push()
+
+void printCoordinates(struct Node *node) {
+    if (!node) {
+        return;
     }
-
-    (*head_ref) = new_node;
-}
-
-void printList(struct Node *node, void (*fptr)(void *))
-{
-    while (node != NULL)
-    {
-        (*fptr)(node->data);
+    while (node->next != NULL) {
+        printf("%f %f\n", node->x_coordinate, node->y_coordinate);
         node = node->next;
     }
-}
+} //printCoordinates()
 
-double randInRange() {
-    return ((rand() % 10000) / 5000.0) - 1;
-}
-
-void printDataPoints(double x_coordinates[], double y_coordinates[]) {
-    printf("Random Points between 0 and 1 to represent points on a graph for and domain and range of 0 to 1.\nX\t\t\tY\n");
-}
-
-int checkDistance(double x, double y) {
-    double sumOfSquares = (x*x) + (y*y);
-    //printf("%f\n", sumOfSquares);
-    if (sqrt(sumOfSquares) <= 1.0) {
-        return 1;
-    } else {
-        return 0;
+void printDistance(struct Node *node) {
+    if (!node) {    //If there is a valid node
+        return;     //If not exit
     }
-}
+    while (node->next != NULL) {    //While not the last node in the LL
+        printf("%f\n", node->distance);     //Prints the distance
+        node = node->next;                  //next Node
+    }
+} //printDistance()
 
-void printInt(void *n) {
-    printf(" %d", *(int *) n);
-}
+int sumInCircle = 0;
+void distanceCalc(struct Node *node) {
+    if (!node) {    //Verify valid node
+        return;
+    }
+    while (node->next != NULL) {
+        float x = node->x_coordinate;   //Sets coordinates while not the last node in LL
+        float y = node->y_coordinate;
+        float sumOfSquares = (x*x) + (y*y); //Square x and y then add
+        node->distance = sqrt(sumOfSquares);    //Square root to find distance from origin
 
+        if (node->distance <= 1) {      //If inside the circle with radius of 1
+            sumInCircle++;  //Increment count of points inside the circle
+        }
+
+        node = node->next;  //Next node
+
+    }
+} //distanceCalc()
+
+
+float randInRange() {
+    //Get rand value from -1 to 1
+    return ((rand() % 10000) / 5000.0) - 1.0;
+}  //randInRange()
 
 int main() {
 
+    //Initialize linked list node
     struct Node *start = NULL;
 
-    unsigned int_size = sizeof(int);
+    //Initialize clock and start timer
+    clock_t t;
+    t = clock();
 
-    double x[NUMBEROFPOINTS];
+    //Reset random number generator
+    srand(time(0));
 
-    for (int i = 0; i < NUMBEROFPOINTS; i++) {
-        x[i] = randInRange();
-        //printf(" %f", x[i]);
-        add(&start, &x[i], int_size);
+    for (int i = 0; i < NUMBEROFPOINTS; i++) {  //For from 0 to the total number of points desired
+        add(&start, randInRange(), randInRange());  //Add random value to x and y to a node
     }
-    printList(start, printInt);
 
-}
+    distanceCalc(start);    //Calc distance from origin from function
+
+
+    //Can use these following statements to see all random values calculated
+    /* ----- */
+
+    //printCoordinates(start);
+    //printDistance(start);
+
+    /* ----- */
+
+    printf("Points inside the circle:\t%d points.\nTotal Points:\t\t\t\t%d points.\n", sumInCircle, NUMBEROFPOINTS);
+
+    /*  From Monte Carlo experiment,
+     *
+     *             4 * (Num inside Circle)
+     *     PI =   ------------------------
+     *              Total Num of Points
+     *
+     */
+
+    printf("Calculated value of PI:\t\t%f\n", 4 * (double)sumInCircle / (double)NUMBEROFPOINTS);
+
+    //End timer
+    t = clock() - t;
+    //Print time taken to console
+    printf("Completed in:\t\t\t\t%f seconds.", (double)t / CLOCKS_PER_SEC);
+
+} //main()
